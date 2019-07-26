cb61f7f31d128d3e674cbc212c4505ec79b21a50
Rob Little <rlittletht@users.noreply.github.com>
2019-07-26 15:43:58 -0700

    update latest test for deploy

--- site-deploy.cmd
+++ site-deploy.cmd
@@ -3,5 +3,40 @@ setlocal
 echo This is my local deploy...
 rem echo Environment:
 rem set
-echo Is deploy on the path?
-where deploy.cmd
+rem echo Is deploy on the path?
+rem  where deploy.cmd
+
+call :CloneOrSyncGit https://github.com/rlittletht/TestReact.git %DEPLOYMENT_SOURCE%\..\SubRepos\TestReactRepo1 production
+call :CloneOrSyncGit https://github.com/rlittletht/TestReact.git %DEPLOYMENT_SOURCE%\..\SubRepos\TestReactRepo2 production
+
+rem ::: Now actually deploy using deploy.cmd
+
+call :DeploySubSite %DEPLOYMENT_SOURCE%\..\SubRepos\TestReactRepo1\NodeJsWebApp1 %DEPLOYMENT_TARGET%\rtest
+call :DeploySubSite %DEPLOYMENT_SOURCE%\..\SubRepos\TestReactRepo1\NodeJsWebApp1 %DEPLOYMENT_TARGET%\rtest3
+
+goto :EOF
+
+rem ::::: CloneOrSyncGit ::::::::
+
+:CloneOrSyncGit
+
+if exist %2 goto SyncGit
+git clone %1 %2 -b %3
+goto :EOF
+
+:SyncGit
+pushd %2
+git pull
+popd
+goto :EOF
+
+rem ::::: DeploySubSite ::::::::
+
+:DeploySubSite
+setlocal
+
+set DEPLOYMENT_SOURCE=%1
+set DEPLOYMENT_TARGET=%2
+
+call deploy.cmd
+goto :EOF
